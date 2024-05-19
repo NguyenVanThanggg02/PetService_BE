85f44d5dfca0302326565654c9330809b22b2b4c
NguyenVanThang <thangnvhe161412@fpt.edu.vn>
2024-05-19 23:34:21 +0700

    data

---
+++ models/comments.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    petId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "pets",
+    },
+    toyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "toys",
+    },
+    medicineId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "medicines",
+    },
+    foodId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "foods",
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Comment = mongoose.model("comments", commentSchema);
+
+export default Comment;
+export { commentSchema };
