c504ea2fbe074145ebb4e76c0def47bb76fb9c5e
ToanNVHE163111 <119161279+ToanNVHE163111@users.noreply.github.com>
2024-05-22 12:54:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Sử dụng module 'express' để khởi tại 1 web server
 import cors from "cors";
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import express, { json } from "express";
 
 import connectDB from "./database.js";
-import { usersRouter } from "./routes/index.js";
+import  { commentRouter, foodRouter, medicineRouter, petRouter, toyRouter, userRouter } from "./routes/index.js";
 dotenv.config();
 //Tạo 1 constant 'app'
 const app = express();
@@ -13,25 +13,29 @@ app.use(cors());
 app.use(json());
 
 //Kích hoạt router hoạt động định tuyến cho các request của client
-app.get("/", (req, res) => {
-  res.send("<h1>Welcom to</h1>");
-});
-app.use("/users", usersRouter);
+app.get('/', (req, res) => {
+    res.send("<h1>Welcom to</h1>")
+})
+app.use('/comments', commentRouter)
+app.use('/pets', petRouter)
+app.use('/users', userRouter)
+app.use('/foods', foodRouter)
+app.use('/toys', toyRouter)
+app.use('/medicines', medicineRouter)
+
+
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-const Port = process.env.PORT || 9999;
+const Port = process.env.PORT || 9999
 
 //Lắng nghe các request gửi tới web server tại port
 
 app.listen(Port, async () => {
-  connectDB();
-  console.log(`web server running on http://localhost:${Port}`);
-});
+    connectDB();
+    console.log(`web server running on http://localhost:${Port}`);
+})
