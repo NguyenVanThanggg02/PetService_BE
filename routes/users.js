1d289685a9e9f88717cc77cf63f48889e26d258d
ToanNVHE163111 <toannvhe163111@fpt.edu.vn>
2024-05-22 12:51:12 +0700

    abc

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,60 @@
-import express from 'express';
-import { userController } from '../controllers/index.js';
-const userRouter = express.Router()
+import createError from "http-errors";
+import User from "../models/user.js";
+import express from "express";
+const usersRouter = express.Router();
 
-userRouter.get('/',userController.getAllUsers)
+// GET: /users -> Lấy tất cả người dùng
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find({}).exec();
+    if (users.length === 0) throw createError(404, "Không tìm thấy người dùng");
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default userRouter
+// GET: /users/:username -> Lấy người dùng theo tên người dùng
+usersRouter.get("/:username", async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const user = await User.findOne({ username: username }).exec();
+    if (!user) throw createError(404, `Người dùng ${username} không tồn tại`);
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST: /users/register -> Đăng ký người dùng mới
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw createError.BadRequest("Yêu cầu nhập tên người dùng và mật khẩu");
+    }
+    const existingUser = await User.findOne({ username: username }).exec();
+    if (existingUser) throw createError.Conflict("Người dùng đã tồn tại");
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.send(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+// POST: /users/login -> Người dùng đăng nhập
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) throw createError.BadRequest("Username is not register");
+    if (password !== user.password) {
+      throw createError.BadRequest("Password is incorrect");
+    }
+    res.status(200).json(user.username);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default usersRouter;
