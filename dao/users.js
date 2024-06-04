d12edf82847ca30bc1cf72c04501e2ef729abdbd
NguyenVanThanggg02 <thang2k210@gmail.com>
2024-06-05 00:23:19 +0700

    changepassword

--- dao/users.js
+++ dao/users.js
@@ -1,23 +1,21 @@
+import User from "../models/user.js";
 
-
-
-
-import User from "../models/user.js"
-
-const fetAllUser = async () =>{
-    try {
-        return await User.find({}).exec()
-    } catch (error) {
-        throw new Error(error.toString())
-    }
-}
+const fetAllUser = async () => {
+  try {
+    return await User.find({}).exec();
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
 
 const forgotPass = async (gmail) => {
-    try {
-      return await User.findOne({ gmail: gmail }).exec();
-    } catch (error) {
-      throw new Error(error.toString());
-    }
-  };
-export default {fetAllUser, forgotPass}
+  try {
+    return await User.findOne({ gmail: gmail }).exec();
+  } catch (error) {
+    throw new Error(error.toString());
+  }
+};
 
+
+
+export default { fetAllUser, forgotPass };
